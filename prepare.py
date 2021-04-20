f48fa4c9680e1e18cebfa2160238914b1d2fc753
Kyle Gorman <kylebgorman@gmail.com>
2021-04-20 14:32:33 -0400

    Update prepare.py

--- prepare.py
+++ prepare.py
@@ -3,36 +3,21 @@
 
 import argparse
 import gzip
-import logging
-import urllib.request
 
 import nltk  # type: ignore
 
 
-DATA_URL = "http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2007.en.shuffled.gz"
-
-
 def main(args: argparse.Namespace) -> None:
-    # This is just a fancy way to download the file from Python.
-    response = urllib.request.urlopen(DATA_URL)
-    with gzip.GzipFile(fileobj=response) as source, open(
+    with gzip.GzipFile(args.gz, "rt") as source, open(
         args.tok, "w"
     ) as sink:
-        for lineno, bline in enumerate(source, 1):
-            # The GzipFile object gives us `bytes`, not `str`.
-            # Therefore we decode before doing further work.
-            line = bline.decode("utf8")
-            # The tokenizer is case-independent so we casefold first.
-            line = line.casefold()
+        for line in source:
             tokens = nltk.word_tokenize(line)
-            print(" ".join(tokens), file=sink)
-            # This logs information every 100k sentences.
-            if not lineno % 100_000:
-                logging.info(f"{lineno:,} sentences processed")
+            print(" ".join(tokens).casefold(), file=sink)
 
-
+            
 if __name__ == "__main__":
-    logging.basicConfig(format="%(levelname)s: %(message)s", level="INFO")
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("tok", help="path for output file")
+    parser.add_argument("gz", help="path to input .gz file")
+    parser.add_argument("tok", help="path for output .tok file")
     main(parser.parse_args())
